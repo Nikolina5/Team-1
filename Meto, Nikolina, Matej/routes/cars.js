8d3029baf0cf0c6a42b23399e188e1e561373fb6
Nikolina5 <eliskovskan@hotmail.com>
2021-08-20 23:19:35 +0200

    Team work

---
+++ Meto, Nikolina, Matej/routes/cars.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('In progress page for cars');
+  // res.render('cars', )
+});
+
+
+
+
+
+module.exports = router;
